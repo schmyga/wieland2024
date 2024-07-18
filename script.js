aebbd558077958311bdc3162b17bd0e89928f3b1
schmyga <93492278+schmyga@users.noreply.github.com>
2024-07-18 22:24:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const menuContainer = document.querySelector('.menu-container');
 menuToggle.addEventListener('click', () => {
   menuContainer.classList.toggle('open');
 });
+window.addEventListener('DOMContentLoaded', function() {
+  var marqueeElement = document.getElementById('marquee');
+  var news = [
+    { theme: '🔴Aktuell:', message: '06.09.2024 Sommerfest vor dem Firmengelände Wieland.' },
+    { theme: '🔴', message: 'Fußball: 1. PLATZ FÜR DAS WIELAND TEAM' }
+  ];
+
+  var marqueeContent = '';
+  news.forEach(function(item) {
+    marqueeContent += <span class="marquee-theme">${item.theme}</span><span class="marquee-message">${item.message}</span> ;
+  });
+
+  marqueeElement.innerHTML = marqueeContent;
+});
