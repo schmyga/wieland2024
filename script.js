2e5039ff1bbcdcce69faeb1fc170a73133b54b82
schmyga <93492278+schmyga@users.noreply.github.com>
2024-07-19 10:42:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-window.addEventListener('DOMContentLoaded', function() {
-  var news = [
-    { theme: '🔴Aktuell:', message: '06.09.2024 Sommerfest vor dem Firmengelände Wieland.' },
-    { theme: '🔴', message: 'Fußball: 1. PLATZ FÜR DAS WIELAND TEAM' }
-  ];
+const menuToggle = document.querySelector('.menu-toggle');
+const menuContainer = document.querySelector('.menu-container');
 
-  var marqueeContent = '';
-  news.forEach(function(item) {
-    marqueeContent += <span class="marquee-theme">${item.theme}</span><span class="marquee-message">${item.message}</span>;
-  });
-
-  var marqueeElement = document.getElementById('news-ticker');
-  marqueeElement.innerHTML = marqueeContent;
-
-  // Обработчик клика на меню-тоглер
-  document.querySelector('.menu-toggle').addEventListener('click', function() {
-    console.log('Клик на меню-тоглер');
-    document.querySelector('.menu').classList.toggle('show');
-  });
+menuToggle.addEventListener('click', () => {
+  menuContainer.classList.toggle('open');
 });
